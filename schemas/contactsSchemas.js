314f2f9017f15f99ac6f76065b4d75348bab6f44
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-07-28 23:02:07 +0200

    controllers and schemas update

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -10,4 +10,6 @@ export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
-});
+})
+  .ruleset.min(1)
+  .rule({ message: "Body must have at least one field" });
